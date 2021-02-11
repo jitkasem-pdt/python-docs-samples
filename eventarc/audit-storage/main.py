c22aa0e730b8850a6cb8c70328feb7875f7b1aed
jitkasem-pdt <69187207+jitkasem-pdt@users.noreply.github.com>
2021-02-11 17:39:06 +0700

    update 1

--- eventarc/audit-storage/main.py
+++ eventarc/audit-storage/main.py
@@ -14,13 +14,11 @@
 
 # [START eventarc_gcs_server]
 import os
-
 from flask import Flask, request
-
+from google.cloud import bigquery
 
 app = Flask(__name__)
 # [END eventarc_gcs_server]
-
 
 # [START eventarc_gcs_handler]
 @app.route('/', methods=['POST'])
@@ -28,6 +26,33 @@ def index():
     # Gets the GCS bucket name from the CloudEvent header
     # Example: "storage.googleapis.com/projects/_/buckets/my-bucket"
     bucket = request.headers.get('ce-subject')
+    
+    # Construct a BigQuery client object.
+    client = bigquery.Client()
+
+    # TODO(developer): Set table_id to the ID of the table to create.
+    table_id = "thailife.cloud_run.test_tbl"
+
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("name", "STRING"),
+            bigquery.SchemaField("post_abbr", "STRING"),
+        ],
+        skip_leading_rows=1,
+        # The source format defaults to CSV, so the line below is optional.
+        source_format=bigquery.SourceFormat.CSV,
+    )
+    
+    uri = "gs://cloud-samples-data/bigquery/us-states/us-states.csv"
+
+    load_job = client.load_table_from_uri(
+        uri, table_id, job_config=job_config
+    )  # Make an API request.
+
+    load_job.result()  # Waits for the job to complete.
+
+    destination_table = client.get_table(table_id)  # Make an API request.
+    # print("Loaded {} rows.".format(destination_table.num_rows))
 
     print(f"Detected change in GCS bucket: {bucket}")
     return (f"Detected change in GCS bucket: {bucket}", 200)
